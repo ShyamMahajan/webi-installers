b20df20bee9e9a12a24b8cd822d737d76f58eaa4
AJ ONeal <aj@therootcompany.com>
2022-08-21 05:43:59 +0000

    f: dash function

--- dash-cli/install.sh
+++ dash-cli/install.sh
@@ -2,7 +2,7 @@
 set -e
 set -u
 
-function __redirect_alias_dashcore() {
+__redirect_alias_dashcore() {
     echo "'dash-cli@${WEBI_TAG:-}' (project) is an alias for 'dashcore@${WEBI_VERSION:-}' (command)"
     WEBI_HOST=${WEBI_HOST:-"https://webinstall.dev"}
     curl -fsSL "$WEBI_HOST/dashcore@${WEBI_VERSION:-}" | bash
